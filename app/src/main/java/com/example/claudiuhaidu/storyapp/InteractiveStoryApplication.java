afb9061a5830ba2fe05fc441f20823c42d451543
claudiuhaidu <haidu_claudiu@yahoo.com>
2018-01-06 14:39:51 +0200

    StoryApp initial commit

---
+++ app/src/main/java/com/example/claudiuhaidu/storyapp/InteractiveStoryApplication.java
@@ -0,0 +1,53 @@
+package com.example.claudiuhaidu.storyapp;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+/**
+ * Created by claudiuhaidu on 26/11/2017.
+ */
+
+public class InteractiveStoryApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
